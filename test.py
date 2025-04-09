b851b6839b01d98a40d19af931e05f440f7cd6a6
Soundarya CS <soundaryacs92@gmail.com>
2025-04-09 16:55:25 +0530

    Edit Key from Windows

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
   response = client.run_instances(
     ImageId='ami-0614680123427b75e',
     InstanceType='t2.medium',
-    KeyName='LINUX',
+    KeyName='WINDOW-EDIT',
     MaxCount=10,
     MinCount=1
 )
