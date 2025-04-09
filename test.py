d2ed35f0f9da2f3540c16d196420a6b71a35e38a
soundaryacs <soundaryacs92@gmail.com>
2025-04-09 11:09:17 +0000

    test.py updates from linux

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
   client = boto3.client('ec2')
   response = client.run_instances(
     ImageId='ami-0614680123427b75e',
-    InstanceType='t2.micro',
+    InstanceType='t2.medium',
     KeyName='cust-key',
     MaxCount=10,
     MinCount=1
