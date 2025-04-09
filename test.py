7fb1084fe82b4bb56d9028e135bcefa646b6a676
Soundarya CS <soundaryacs92@gmail.com>
2025-04-09 16:34:52 +0530

    test.py file updates

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+import linux
+import boto3
 
+def lambda_handler(event, context):
+  client = boto3.client('ec2')
+  response = client.run_instances(
+    ImageId='ami-0614680123427b75e',
+    InstanceType='t2.micro',
+    KeyName='cust-key',
+    MaxCount=1,
+    MinCount=1
+)
